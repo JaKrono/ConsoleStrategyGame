c459afacd5b88d5322f3caa3c8bf47774d7cc62f
Jack Krono <62642934+JaKrono@users.noreply.github.com>
2020-04-24 10:32:20 +0430

    Update Player.h

--- Files/HeaderFiles/Player.h
+++ Files/HeaderFiles/Player.h
@@ -2,6 +2,8 @@
 #define PLAYER_H
 
 #include <string>
+#include <iostream>
+
 #include "City.h"
 #include "Army.h"
 
@@ -10,25 +12,35 @@ private:
 	int foodStock;
 	int goldStock;
 
-	Army playersArmy;
+	Army playersArmy[4]; // Pikeman, Knight, Archer, Swordsman
 	int spyCount;
+	int spyFoodCost;
+	int spyGoldCost;
+
 public:
 	int getFoodStock();
 	int getGoldStock();
-	int getSoldierCount();
 	int getSpyCount();
+	void setFoodStock(int a);
+	void setGoldStock(int a);
+	void setSpyCount(int a);
+
+	void setValues(int difficulty); // difficulty: 1-Easy / 2-Hard
 
 	// returns true if the city has been captured and false if not
-	bool attack(int attackingTroops, City* theCity);
+	bool attack(City* theCity);
 
 	// returns true if resources are available for creating this amount of troops 
-	// and false if not. Type for soldier is 1 and for spy is 2
-	bool makeTroops(int number, int type);
+	// and false if not.
+	void makeTroops();
 
 	// returns true if a spy can be sent to the towm:
 	// 1. there are no spies already in that town
 	// 2. that town is an enemy
 	// 3. there is at least 1 spy available
 	bool sendSpy(City* theCity);
+
+	// handles input for attacking and making troops
+	int* inputArmy(std::string type); // type = "Attack" , "Make"
 };
 #endif // Player.h
